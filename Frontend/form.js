369f74fcaca23e52d6e45338a4fd37fcd1011510
Kashif <57903864+Chocovanilla7@users.noreply.github.com>
2021-04-21 10:36:31 +0530

    Add files via upload

---
+++ Frontend/form.js
@@ -0,0 +1,8 @@
+
+function openform() {
+    document.getElementById("myform").style.display = "block";
+}
+
+function closeform() {
+    document.getElementById("myform").style.display = "none";
+}
